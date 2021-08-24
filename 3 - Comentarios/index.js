b81b7218128f7053f4f957c35b416fedfc4ed488
Eduardo <54048170+Eduardo-ops@users.noreply.github.com>
2021-08-24 09:31:12 -0300

    Forma correta de comentar codigo adicionado

---
+++ 3 - Comentarios/index.js
@@ -0,0 +1,12 @@
+// COMENTÁRIO DE APENAS UMA LINHA
+
+// Caixa simples de mensagem
+alert('Exemplo de comentários de apenas uma linha')
+
+// COMENTÁRIO DE VÁRIAS LINHAS
+
+/* 
+    Seleciona um elemento do DOM pelo id
+    Atualiza o valor desse elemento com uma string
+*/
+document.getElementById('id').value = 'Valor teste'
