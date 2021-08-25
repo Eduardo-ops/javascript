2b5d70cf15b88dfdf89bb73c43b98a504c6683fb
Eduardo <54048170+Eduardo-ops@users.noreply.github.com>
2021-08-25 16:17:38 -0300

    Adicionado variaveis

---
+++ 4 - Variaveis/index.js
@@ -0,0 +1,24 @@
+
+// string
+var nome = 'Eduardo Isidoro Gonçalves'
+
+//number
+var numeroInteiro = 24
+
+// float - double
+var numeroFracionario = 17.43
+
+// boolean
+var status = true // True = Verdadeiro / False = falso
+
+// Muito utilizado em debug
+alert(numeroFracionario)
+
+// Método que possibilita gravar sequência de caracteres em um documento aberto.
+document.write(nome)
+
+/* 
+    Método que permite exibir uma mensagem no console do navegador e na ide para teste
+    Muito utilizado para debug 
+*/
+console.log(numeroInteiro)
