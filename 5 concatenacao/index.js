7e43e6d667336c3416a12e10793dd4de38991c43
Eduardo <54048170+Eduardo-ops@users.noreply.github.com>
2021-08-26 16:05:59 -0300

    Concatenacao adicionado

---
+++ 5 concatenacao/index.js
@@ -0,0 +1,6 @@
+// Caixa de diálogo com mensagem opcional, solicitando entrada de algum texto.
+var nome = prompt('Digite seu nome:')
+var curso = 'Javascript'
+
+// Concatenação
+document.write('<h1>Bem vindo ' + nome + ' ao curso de ' + curso + '</h1>')
