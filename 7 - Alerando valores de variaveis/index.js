4d8dbc8bbd404e9d6ad97526df64a339d95bb857
Eduardo <54048170+Eduardo-ops@users.noreply.github.com>
2021-08-27 16:14:51 -0300

    Alteracao de valor de variaveis adicionado

---
+++ 7 - Alerando valores de variaveis/index.js
@@ -0,0 +1,10 @@
+  var teste = null
+console.log(teste)
+
+// Variável com seu valor alterado de null para uma string
+teste = 'Valor alterado com sucesso!!!'
+console.log(teste)
+
+// Variável novamente com seu valor alterado, sendo anteriormente na linha 5 uma string e agora do tipo number/int
+teste = 120
+console.log(teste)
