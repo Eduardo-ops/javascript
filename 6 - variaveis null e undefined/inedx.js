91b72cc160ba3e8b0e6a2cde88bc7b91b821e7aa
Eduardo <54048170+Eduardo-ops@users.noreply.github.com>
2021-08-27 16:02:36 -0300

    Variaveis null e undefined adicionada

---
+++ 6 - variaveis null e undefined/inedx.js
@@ -0,0 +1,12 @@
+/* 
+Variável null
+Variáveis desse tipo são diferentes de zero(0), diferente de string("") e diferente de uma variável 
+que não foi inicializada(empty)
+*/
+var teste = null
+
+/* 
+Variável undefined
+Variável que não teve valor indefinido.
+*/
+var teste2
