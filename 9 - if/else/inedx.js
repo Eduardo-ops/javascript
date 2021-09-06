5d17911240644733704d618af96b38b2d4728748
Eduardo <54048170+Eduardo-ops@users.noreply.github.com>
2021-09-06 19:16:03 -0300

    Condicoes if else implementado

---
+++ 9 - if/else/inedx.js
@@ -0,0 +1,57 @@
+// Sintaxe if
+if (condicao) {
+    // Toda lógica a ser executada
+}
+
+// Sintaxe if else
+if (condicao) {
+    // Toda lógica a ser executada
+} else {
+    // Toda lógica a ser executada
+}
+
+// Três ou mais condições existentes
+if (condicao) {
+    // Toda lógica a ser executada
+} else if (condicao2) {
+    // Toda lógica a ser executada
+} else {
+    // Toda lógica a ser executada
+}
+
+// Variável do tipo string com valor atribuído
+semafaro = "verde"
+
+// Condição de igualdade, se o valor de variável for igual a valor sugerido na condição
+if (semafaro == "verde") {
+    mesagem = "Pode passar"
+}
+
+semafaro = "vermelho"
+
+if (semafaro == "verde") {
+    mensagem = "Pode passar"
+} else {
+    mensagem = "Pare"
+}
+
+semafaro = amarelo
+
+if (semafaro == "verde") {
+    mensagem = "Pode passar"
+} else if (mensagem == "Vermelho") {
+    mensagem = "Pare"
+} else {
+    mensagem = "Atenção"
+}
+
+username = "usuario123"
+password = "123456"
+
+if (username == "usuario123" && password == "123456") {
+    console.log('Usuário autenticado')
+} else {
+    console.log('Usuário e/ou senha incorretos')
+}
+
+
