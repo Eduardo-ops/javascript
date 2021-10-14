e5ee8ec155ed0c21c85c54436acea667f112c2e4
Eduardo <54048170+Eduardo-ops@users.noreply.github.com>
2021-10-14 15:46:09 -0300

    Casting dos tipos toString, parseInt e parseFloat implementado

---
+++ 10 - casting, to string, parseInt e parseFloat/index.js
@@ -0,0 +1,28 @@
+// Parseando de string para int
+
+// Variáveis com valores do tipo string
+var v1 = '1'
+var v2 = '2'
+
+// Transformando variáveis com valor string para valor int
+v1 = parseInt(v1)
+v2 = parseInt(v2)
+
+// Realizando soma nesse caso
+console.log(v1 + v2)
+
+// Parseando de string para float
+var v3 = '1.23'
+var v4 = '2.45'
+
+v3 = parseFloat(v3)
+v4 = parseFloat(v4)
+
+console.log(v3 = v4)
+
+// Representando variáveis do tipo int como string
+var v5 = 20
+var v6 = 21
+
+// Concatenando
+console.log('Estamos em ' + v5.toString() + v6.toString())
