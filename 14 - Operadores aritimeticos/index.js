72e136628818f6a109c972e440ac41db7ebe2600
Eduardo <54048170+Eduardo-ops@users.noreply.github.com>
2021-10-18 13:55:31 -0300

    Operadores aritimeticos implementado

---
+++ 14 - Operadores aritimeticos/index.js
@@ -0,0 +1,13 @@
+var number1 = '5'
+var number2 = '10'
+
+number1 = parseInt(number1)
+number2 = parseInt(number2)
+
+console.log('A soma entre ' + number1 + ' e ' + number2 + ' é igual a: ' + (number1 + number2))
+console.log('A subtração entre ' + number1 + ' e ' + number2 + ' é igual a: ' + (number1 - number2))
+console.log('A multiplicação entre ' + number1 + ' e ' + number2 + ' é igual a: ' + (number1 * number2))
+console.log('A divisão entre ' + number1 + ' e ' + number2 + ' é igual a: ' + (number1 / number2))
+console.log('O módulo entre ' + number1 + ' e ' + number2 + ' é igual a: ' + (number1 % number2))
+
+
