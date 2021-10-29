9c8e73c50e6766318d85debcf9614c530d87edfa
Eduardo Isidoro <eduardof013@hotmail.com>
2021-10-29 09:41:26 -0300

    Operacoes aritmeticas com atribuicoes implementado

---
+++ 15 - Operações aritméticas e suas atribuições/index.js
@@ -0,0 +1,33 @@
+var teste = 10
+
+// Ooperação comum
+teste = teste + 5
+console.log(teste)
+
+// Operações com processo de atribuições
+teste += 5
+console.log(teste)
+
+teste -= 5
+console.log(teste)
+
+teste *= 5
+console.log(teste)
+
+teste /= 5
+console.log(teste)
+
+teste %= 5
+console.log(teste)
+
+// Concatenação de strings utilizando processo de atribuições
+
+var nome = 'Eduardo'
+
+nome += 'Isidoro'
+
+console.log(nome)
+
+
+
+
