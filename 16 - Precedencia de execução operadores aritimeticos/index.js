18df6b1a45a85ababe80475a3d96d74bb89d3546
Eduardo Isidoro <eduardof013@hotmail.com>
2021-11-01 08:53:17 -0300

    Precedencia de execucao adicionado

---
+++ 16 - Precedencia de execução operadores aritimeticos/index.js
@@ -0,0 +1,12 @@
+/* 
+    1º Multiplicação
+    2º Divisão
+    3º Soma
+    4º Subtração
+*/
+console.log(11 + 5 - 5 * 2 / 2)
+
+/* 
+    (- Aplicar precedência)
+*/
+console.log((11 + 5 - 5 * 2) / 2)
