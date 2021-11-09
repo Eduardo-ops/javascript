9400f080bb150d808ff58bb2c4b6342e46be09ac
Eduardo Isidoro <eduardof013@hotmail.com>
2021-11-09 11:39:19 -0300

    Funcoes anonimas implementado

---
+++ 21 - Funcao anonima e tecnica wrapper/index.js
@@ -0,0 +1,10 @@
+/* 
+    Função anônima é uma função sem nome, geralmente não é acessível após sua criação.
+*/
+
+//Sintaxe
+var saudacao = function (nome) {
+    document.write('Olá ' + nome + ' tudo bem ?')
+}
+
+saudacao('Eduardo')
