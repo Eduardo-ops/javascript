6ecfbce909743c0e8770533b1706983c0548f90f
eduardo-ops <eduardof013@hotmail.com>
2021-11-09 13:48:29 -0300

    Funcao de callback iniciada

---
+++ 22 - Funcao de callback/index.js
@@ -0,0 +1,7 @@
+function validationId(id, successCallback, errorCallback) {
+    if (true) {
+        successCallback('Operation executed with success')
+    } else {
+        errorCallback('Operation executed with error')
+    }
+}
