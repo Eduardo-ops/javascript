4f73fe6c0118c9dce868c38bf4e16d8ebca54a3c
eduardo-ops <eduardof013@hotmail.com>
2021-11-10 14:08:48 -0300

    Funcao de callback iniciada

--- 22 - Funcao de callback/index.js
+++ 22 - Funcao de callback/index.js
@@ -1,7 +1,20 @@
-function validationId(id, successCallback, errorCallback) {
+function exibirArtigo(id, successCallback, errorCallback) {
+    // Lógica para recuperar id
     if (true) {
         successCallback('Operation executed with success')
     } else {
         errorCallback('Operation executed with error')
     }
 }
+
+var successCallback = function (title, description) {
+    document.write('<h1>' + title + '</h1>')
+    document.write('<hr>')
+    document.write('<p>' + description + '</p>')
+}
+
+var errorCallback = function (message) {
+    document.write('<p>' + message + '</p>')
+}
+
+exibirArtigo(8, successCallback, errorCallback)
