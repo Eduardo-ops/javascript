36cdd711614b0a4f9a4f6493ef2dc3f0a9346b0a
Eduardo Isidoro <eduardof013@hotmail.com>
2021-11-11 09:41:26 -0300

    Funcoes nativas para manipulacao de datas

---
+++ 25 - Funcoes nativas para manipular datas/index.js
@@ -0,0 +1,14 @@
+// Instância de um objeto Date
+var data = new Date()
+
+// Recuperar a data do exato momento de execução de código
+document.write(data.getDate())
+document.write('<br>')
+
+// Recuperar a o mês do exato momento de execução de código
+document.write(data.getMonth() + 1)
+document.write('<br>')
+
+// Recuperar a o ano do exato momento de execução de código
+document.write(data.getFullYear())
+document.write('<br>')
