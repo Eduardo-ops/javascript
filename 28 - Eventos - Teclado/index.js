d871574077473eede05b51be5c5a92dabd0c99ed
Eduardo Isidoro <eduardof013@hotmail.com>
2021-11-16 11:45:14 -0300

    Eventos com acionamento pelo teclado

---
+++ 28 - Eventos - Teclado/index.js
@@ -0,0 +1,11 @@
+function action() {
+    alert('Evento disparado!')
+}
+
+function test1() {
+    alert('O element foi clicado!')
+}
+
+function test2() {
+    alert('O cursos do mouse deixou o elemento!')
+}
